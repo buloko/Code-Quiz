b726b8ce7fb1c76025137e2d6411c4d73ece6c76
buloko <buloko8@gmail.com>
2022-11-03 00:48:27 -0400

    commit to CODE

---
+++ script.js
@@ -0,0 +1,6 @@
+var startButtonEl = document.getElementById('start-button');
+
+startButtonEl.addEventListener('click', function(){
+    console.log('test');
+});
+<startButtonEl onclick= "functionToExecute()"></startButtonEl>
