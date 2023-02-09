1a9d9f99e6cb21331e84870d3b2fb362e4b6ec20
buloko <buloko8@gmail.com>
2023-02-09 13:07:33 -0500

    syntax getting rid of usless idenitifer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var timerEL = document.querySelector("#time");
-var startButtonEl = document.getElementById('start-button');
-var startAreaEl = document.getElementById('starter-area');
+var startButton = document.getElementById('start-button');
+var startArea = document.getElementById('starter-area');
 var endScreenEl = document.getElementById("#end-screen");
 var submitButton = document.getElementById("#submit")
 
@@ -190,120 +190,142 @@ function pickTheAnswer() {
     endScreenEl.classList.remove("hide");
     finalScoreEl.textConten = timeLeft;
    }
-//peform repeatedly
-function iterate(id) {
-
-    //Getting the highscore display section
-    var highscore = document.getElementsByClassName("highscore");
-    // highscore[0].innerText = "";
-
-    //Getting the question
-    const question = document.getElementById("question");
-
-    //setting the question text
-    question.innerText = Questions[id].question;
-
-    //getting the question options
-    const op1 = document.getElementById('op1');
-    const op2 = document.getElementById('op2');
-    const op3 = document.getElementById('op3');
-    const op4 = document.getElementById('op4');
-
-
-    // Providing option text 
-    op1.innerText = Questions[id].choices[0].text;
-    op2.innerText = Questions[id].choices[1].text;
-    op3.innerText = Questions[id].choices[2].text;
-    op4.innerText = Questions[id].choices[3].text;
-    op1.addEventListener("click",checkAnswer)
-    op2.addEventListener("click",checkAnswer)
-    op3.addEventListener("click",checkAnswer)
-    op4.addEventListener("click",checkAnswer)
-
-    // Providing the true or false value to the options
-    op1.value = Questions[id].choices[0].isCorrect;
-    op2.value = Questions[id].choices[1].isCorrect;
-    op3.value = Questions[id].choices[2].isCorrect;
-    op4.value = Questions[id].choices[3].isCorrect;
-
-
-    var selected = "";
-
-    // Show selection for op1
-    op1.addEventListener("click", () => {
-        op1.style.backgroundColor = "yellow";
-        op2.style.backgroundColor = "darkblue";
-        op3.style.backgroundColor = "red";
-        op4.style.backgroundColor = "purple";
-        selected = op1.value;
-    })
-
-    // Show selection for op2
-    op2.addEventListener("click", () => {
-        op1.style.backgroundColor = "red";
-        op2.style.backgroundColor = "yellow";
-        op3.style.backgroundColor = "purple";
-        op4.style.backgroundColor = "darkblue";
-        selected = op2.value;
-    })
-
-    // Show selection for op3
-    op3.addEventListener("click", () => {
-        op1.style.backgroundColor = "red";
-        op2.style.backgroundColor = "darkblue";
-        op3.style.backgroundColor = "yellow";
-        op4.style.backgroundColor = "purple";
-        selected = op3.value;
-    })
-
-    // Show selection for op4
-    op4.addEventListener("click", () => {
-        op1.style.backgroundColor = "red";
-        op2.style.backgroundColor = "darkblue";
-        op3.style.backgroundColor = "yellow";
-        op4.style.backgroundColor = "purple";
-        selected = op4.value;
-    })
-    
-    // Grabbing the evaluate button
-    const evaluate = document.getElementsByClassName("evaluate");
-
-    // Evaluate method
-    evaluate[0].addEventListener("click", () => {
-        if (selected == "true") {
-            result[0].innerHTML = "True";
-            result[0].style.color = "green";
-        } else {
-            result[0].innerHTML = "False";
-            result[0].style.color = "red";
+   function saveHighscore(event) {
+    var putName = putNameEl.value
+    console.log(putName);
+    var highScore= [{name: putName, score: timeLeft}];
+    console.log(highScore);
+    localStorage.setItem('High-Scores'), JSON.stringify(highScore)
+    putNameEl.value = '';
+}
+//timer
+function startTimer () {
+    let timeLeft = timer;
+    timerInterval = setInterval (() => {
+        timeLeft --;
+        document.querySelector("timer").textContent = timeLeft
+        
+        if(timeLeft===0) {
+            endQuiz();
         }
-    })
+    },1000);
 }
-var currentQuestion = 0
-if (start) {
-    iterate(currentQuestion);
-}
+startButt
 
-// Next button and method
-const next = document.getElementsByClassName('next')[0];
-var id = 0;
+//peform repeatedly
+// function iterate(id) {
+    
+    //     //Getting the highscore display section
+    //     var highscore = document.getElementsByClassName("highscore");
+    //     // highscore[0].innerText = "";
+    
+    //     //Getting the question
+    //     const question = document.getElementById("question");
+    
+    //     //setting the question text
+    //     question.innerText = Questions[id].question;
 
-next.addEventListener("click", () => {
-    start = false;
-    if (id < 2) {
-        id++;
-        iterate(id);
-        console.log(id);
-    }
+//     //getting the question options
+//     const op1 = document.getElementById('op1');
+//     const op2 = document.getElementById('op2');
+//     const op3 = document.getElementById('op3');
+//     const op4 = document.getElementById('op4');
 
-})
 
-function checkAnswer(event){
-    // console.log(Questions[currentQuestion].choices.value)
-    console.log(typeof event.target.value)
-    currentQuestion++
-    iterate(currentQuestion)
-}
+//     // Providing option text 
+//     op1.innerText = Questions[id].choices[0].text;
+//     op2.innerText = Questions[id].choices[1].text;
+//     op3.innerText = Questions[id].choices[2].text;
+//     op4.innerText = Questions[id].choices[3].text;
+//     op1.addEventListener("click",checkAnswer)
+//     op2.addEventListener("click",checkAnswer)
+//     op3.addEventListener("click",checkAnswer)
+//     op4.addEventListener("click",checkAnswer)
+
+//     // Providing the true or false value to the options
+//     op1.value = Questions[id].choices[0].isCorrect;
+//     op2.value = Questions[id].choices[1].isCorrect;
+//     op3.value = Questions[id].choices[2].isCorrect;
+//     op4.value = Questions[id].choices[3].isCorrect;
+
+
+//     var selected = "";
+
+//     // Show selection for op1
+//     op1.addEventListener("click", () => {
+//         op1.style.backgroundColor = "yellow";
+//         op2.style.backgroundColor = "darkblue";
+//         op3.style.backgroundColor = "red";
+//         op4.style.backgroundColor = "purple";
+//         selected = op1.value;
+//     })
+
+//     // Show selection for op2
+//     op2.addEventListener("click", () => {
+//         op1.style.backgroundColor = "red";
+//         op2.style.backgroundColor = "yellow";
+//         op3.style.backgroundColor = "purple";
+//         op4.style.backgroundColor = "darkblue";
+//         selected = op2.value;
+//     })
+
+//     // Show selection for op3
+//     op3.addEventListener("click", () => {
+//         op1.style.backgroundColor = "red";
+//         op2.style.backgroundColor = "darkblue";
+//         op3.style.backgroundColor = "yellow";
+//         op4.style.backgroundColor = "purple";
+//         selected = op3.value;
+//     })
+
+//     // Show selection for op4
+//     op4.addEventListener("click", () => {
+//         op1.style.backgroundColor = "red";
+//         op2.style.backgroundColor = "darkblue";
+//         op3.style.backgroundColor = "yellow";
+//         op4.style.backgroundColor = "purple";
+//         selected = op4.value;
+//     })
+    
+//     // Grabbing the evaluate button
+//     const evaluate = document.getElementsByClassName("evaluate");
+
+//     // Evaluate method
+//     evaluate[0].addEventListener("click", () => {
+//         if (selected == "true") {
+//             result[0].innerHTML = "True";
+//             result[0].style.color = "green";
+//         } else {
+//             result[0].innerHTML = "False";
+//             result[0].style.color = "red";
+//         }
+//     })
+// }
+// var currentQuestion = 0
+// if (start) {
+//     iterate(currentQuestion);
+// }
+
+// // Next button and method
+// const next = document.getElementsByClassName('next')[0];
+// var id = 0;
+
+// next.addEventListener("click", () => {
+//     start = false;
+//     if (id < 2) {
+//         id++;
+//         iterate(id);
+//         console.log(id);
+//     }
+
+// })
+
+// function checkAnswer(event){
+//     // console.log(Questions[currentQuestion].choices.value)
+//     console.log(typeof event.target.value)
+//     currentQuestion++
+//     iterate(currentQuestion)
+// }
 
 //unhide quiz 
 
@@ -318,16 +340,3 @@ function checkAnswer(event){
 
 
 //getQuestion
-
-//timer
-function startTimer () {
-    let timeLeft = timer;
-    timerInterval = setInterval (() => {
-        timeLeft --;
-        document.querySelector("timer").textContent = timeLeft
-        
-        if(timeLeft===0) {
-            endQuiz();
-        }
-    },1000);
-}
