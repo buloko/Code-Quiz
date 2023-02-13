a469e8005ce38e9f8da70d3100630be497a31b6d
buloko <buloko8@gmail.com>
2023-02-12 23:54:07 -0500

    trying to fix the pick the answer function

--- js/script.js
+++ js/script.js
@@ -23,60 +23,60 @@ var questions = [
     // id: 0,
     question: "What was first commercially sucessful videogame?",
     answers: [
-      { text: "Pong", isCorrect: true },
-      { text: "Mario", isCorrect: false },
-      { text: "Rock,Paper,Scissors", isCorrect: false },
-      { text: "Space Race", isCorrect: false },
+      { text: "Pong", correct: true },
+      { text: "Mario", correct: false },
+      { text: "Rock,Paper,Scissors", correct: false },
+      { text: "Space Race", correct: false },
     ],
   },
   {
     // id: 1,
     question: "What was the best selling game of all time?",
     answers: [
-      { text: "Mario", isCorrect: false },
-      { text: "Fifa", isCorrect: false },
-      { text: "Minecraft", isCorrect: true },
-      { text: "Super Smash Brawl", isCorrect: false },
+      { text: "Mario", correct: false },
+      { text: "Fifa", correct: false },
+      { text: "Minecraft", correct: true },
+      { text: "Super Smash Brawl", correct: false },
     ],
   },
   {
     // id: 2,
     question: "What kind of animal is Sonic?",
     answers: [
-      { text: "Alligator", isCorrect: false },
-      { text: "Echindna", isCorrect: false },
-      { text: "Hedgehog", isCorrect: true },
-      { text: "Badger", isCorrect: false },
+      { text: "Alligator", correct: false },
+      { text: "Echindna", correct: false },
+      { text: "Hedgehog", correct: true },
+      { text: "Badger", correct: false },
     ],
   },
   {
     // id: 3,
     question: "When was the PS5 released?",
     answers: [
-      { text: "November 2020", isCorrect: true },
-      { text: "June 2019", isCorrect: false },
-      { text: "December 2021", isCorrect: false },
-      { text: "July 2022", isCorrect: false },
+      { text: "November 2020", correct: true },
+      { text: "June 2019", correct: false },
+      { text: "December 2021", correct: false },
+      { text: "July 2022", correct: false },
     ],
   },
   {
     // id: 4,
     question: "What is the armored vehicle in Halo?",
     answers: [
-      { text: "Blue Falcon", isCorrect: false },
-      { text: "Ice Cream Truck", isCorrect: false },
-      { text: "Warthog", isCorrect: true },
-      { text: "Batmobile", isCorrect: false },
+      { text: "Blue Falcon", correct: false },
+      { text: "Ice Cream Truck", correct: false },
+      { text: "Warthog", correct: true },
+      { text: "Batmobile", correct: false },
     ],
   },
   {
     // id: 5,
     question: "Which company makes the best console?",
     answers: [
-      { text: "Sony(Playstation", isCorrect: true },
-      { text: "Mircosoft(Xbox)", isCorrect: false },
-      { text: "Nintendo(Switch)", isCorrect: false },
-      { text: "PC(any computer really)", isCorrect: false },
+      { text: "Sony(Playstation", correct: true },
+      { text: "Mircosoft(Xbox)", correct: false },
+      { text: "Nintendo(Switch)", correct: false },
+      { text: "PC(any computer really)", correct: false },
     ],
   },
   {
@@ -84,30 +84,30 @@ var questions = [
     question:
       "Which game is widely regarded as the worst videogame ever created?",
     answers: [
-      { text: "Atari ET", isCorrect: true },
-      { text: "Last of Us 2", isCorrect: false },
-      { text: "Postal 2", isCorrect: false },
-      { text: "Night Trap", isCorrect: false },
+      { text: "Atari ET", correct: true },
+      { text: "Last of Us 2", correct: false },
+      { text: "Postal 2", correct: false },
+      { text: "Night Trap", correct: false },
     ],
   },
   {
     // id: 7,
     question: "Who invented the video game?",
     answers: [
-      { text: "William Higinbotham", isCorrect: true },
-      { text: "Bobby Kotick", isCorrect: false },
-      { text: "Andrew Wilson", isCorrect: false },
-      { text: "Strauss Zelnick", isCorrect: false },
+      { text: "William Higinbotham", correct: true },
+      { text: "Bobby Kotick", correct: false },
+      { text: "Andrew Wilson", correct: false },
+      { text: "Strauss Zelnick", correct: false },
     ],
   },
   {
     // id: 8,
     question: "Where are Nintendo Original headquarters located?",
     answers: [
-      { text: "Los Angeles", isCorrect: false },
-      { text: "New York", isCorrect: false },
-      { text: "Kyoto", isCorrect: true },
-      { text: "Shanghai", isCorrect: false },
+      { text: "Los Angeles", correct: false },
+      { text: "New York", correct: false },
+      { text: "Kyoto", correct: true },
+      { text: "Shanghai", correct: false },
     ],
   },
   {
@@ -115,10 +115,10 @@ var questions = [
     question:
       "What was the relation of Kratos with Zeus in the game God of War?",
     answers: [
-      { text: "Father", isCorrect: false },
-      { text: "Cousin", isCorrect: false },
-      { text: "Son", isCorrect: true },
-      { text: "Brother", isCorrect: false },
+      { text: "Father", correct: false },
+      { text: "Cousin", correct: false },
+      { text: "Son", correct: true },
+      { text: "Brother", correct: false },
     ],
   },
   {
@@ -126,10 +126,10 @@ var questions = [
     question:
       "What year was the PlayStation Portable first released in the USA?",
     answers: [
-      { text: "2019", isCorrect: false },
-      { text: "2005", isCorrect: false },
-      { text: "2004", isCorrect: true },
-      { text: "2023", isCorrect: false },
+      { text: "2019", correct: false },
+      { text: "2005", correct: false },
+      { text: "2004", correct: true },
+      { text: "2023", correct: false },
     ],
   },
 ];
@@ -163,7 +163,7 @@ function renderQuestion(question) {
     choiceButton.textContent = answer.text;
     choiceButton.classList.add("button");
     if (answerEl.isCorrect) {
-      choiceButton.dataset.isCorrect = answer.isCorrect;
+      choiceButton.dataset.correct = "true";
     }
     answerEl.appendChild(choiceButton);
     choiceButton.addEventListener("click", pickTheAnswer);
@@ -171,10 +171,11 @@ function renderQuestion(question) {
 }
 
 function pickTheAnswer() {
-  var dataCorrect = this.dataset.isCorrect;
+  var isCorrect = this.dataset.correct === "true";
 
-  if (dataCorrect) {
+  if (isCorrect) {
     messageEl.textContent = "Correct";
+    timeLeft = timeLeft + 10
   } else {
     messageEl.textContent = "Wrong";
     timeLeft = timeLeft - 10;
